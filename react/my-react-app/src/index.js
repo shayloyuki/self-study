26e23ee30fbc07243987a0e34dbf657db96b2899
shayloyuki <shayluo@icloud.com>
2022-04-21 14:49:57 +0800

    begin to learn react from w3school

---
+++ react/my-react-app/src/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: luoxue
+ * @Date: 2022-04-21 12:50:55
+ * @LastEditors: luoxue
+ * @LastEditTime: 2022-04-21 13:11:14
+ * @Description: 
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const myfirstelement = <h1>Hello World!</h1>
+
+ReactDOM.render(myfirstelement, document.getElementById('root'));
